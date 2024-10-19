9462cefba3f384b1c3e919da1b20152904793327
Robert Alex Barbu <robertalexbarbu@gmail.com>
2024-10-19 16:45:03 +0300

    Test

--- GameOfLife/src/main/java/com/example/gameoflife/GameOfLifeApplication.java
+++ GameOfLife/src/main/java/com/example/gameoflife/GameOfLifeApplication.java
@@ -2,12 +2,24 @@ package com.example.gameoflife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GameOfLifeApplication {
 
+    // Swaggger url: http://localhost:8080/swagger-ui/index.html#/
     public static void main(String[] args) {
         SpringApplication.run(GameOfLifeApplication.class, args);
+    }
+
+    @RestController
+    public class HelloWorldController {
+
+        @GetMapping("/test2")
+        public String helloWorld() {
+            return "HelloWorld";
+        }
     }
 
 }
